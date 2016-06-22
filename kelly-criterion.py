442d841010d9e3c6a432e2d08e1b8d42c271bd52
Tibor Kiss <tibor.kiss@gmail.com>
2016-06-22 09:06:03 +0200

    PEP8 style changes

--- kelly-criterion.py
+++ kelly-criterion.py
@@ -28,7 +28,6 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 
-
 """Kelly Criterion - (c) 2014-2015, Tibor Kiss <tibor.kiss@gmail.com>
 
 Usage:
@@ -49,7 +48,8 @@ from pandas import DataFrame
 
 from numpy.linalg import inv
 
-def calc_kelly_leverages(securities, startDate, endDate, riskFreeRate=0.04):
+
+def calc_kelly_leverages(securities, start_date, end_date, risk_free_rate=0.04):
     """Calculates the optimal leverages for the given securities and time frame.
     Returns a list of (security, leverage) tuple with the calculate optimal leverages.
 
@@ -57,25 +57,24 @@ def calc_kelly_leverages(securities, startDate, endDate, riskFreeRate=0.04):
     """
     f = {}
     ret = {}
-    excRet = {}
+    excess_return = {}
 
     # Download the historical prices from Yahoo Finance and calculate the
     # excess return (return of security - risk free rate) for each security.
     for symbol in securities:
         try:
-            hist_prices = web.DataReader(symbol, 'yahoo', startDate, endDate)
+            hist_prices = web.DataReader(symbol, 'yahoo', start_date, end_date)
         except IOError, e:
             print 'Unable to download data for %s. Reason: %s' % (symbol, str(e))
             return None
 
         f[symbol] = hist_prices
 
-        # ret[symbol] = (hist_prices['Adj Close'] - hist_prices['Adj Close'].shift(1)) / hist_prices['Adj Close'].shift(1)
         ret[symbol] = hist_prices['Adj Close'].pct_change()
-        excRet[symbol] = (ret[symbol] - (riskFreeRate/252))  # RiskFreeRate is annualized
+        excess_return[symbol] = (ret[symbol] - (risk_free_rate / 252))  # risk_free_rate is annualized
 
     # Create a new DataFrame based on the Excess Returns.
-    df = DataFrame(excRet).dropna()
+    df = DataFrame(excess_return).dropna()
 
     # Calculate the CoVariance and Mean of the DataFrame
     C = 252 * df.cov() 
@@ -108,22 +107,22 @@ def main(argv):
 
     # Parse start and end dates
     try:
-        startDate = datetime.strptime(args['<start-date>'], "%Y-%m-%d").date()
+        start_date = datetime.strptime(args['<start-date>'], "%Y-%m-%d").date()
     except ValueError, e:
         print 'Error parsing start-date: %s' % args['<start-date>']
         sys.exit(-1)
 
     try:
-        endDate = datetime.strptime(args['<end-date>'], "%Y-%m-%d").date()
+        end_date = datetime.strptime(args['<end-date>'], "%Y-%m-%d").date()
     except ValueError, e:
         print 'Error parsing end-date: %s' % args['<start-date>']
         sys.exit(-1)
 
-    print 'Arguments: risk-free-rate=%s start-date=%s end-date=%s securities=%s' % (args['--risk-free-rate'], startDate, endDate, args['<security>'])
+    print 'Arguments: risk-free-rate=%s start-date=%s end-date=%s securities=%s' % (args['--risk-free-rate'], start_date, end_date, args['<security>'])
     print ''
 
     # Calculate the Kelly Optimal leverages
-    leverages = calc_kelly_leverages(args['<security>'], startDate, endDate, riskFreeRate)
+    leverages = calc_kelly_leverages(args['<security>'], start_date, end_date, riskFreeRate)
 
     # Print the results if calculation was successful
     if leverages: 
@@ -132,7 +131,6 @@ def main(argv):
             print "  %s: %.2f" % (name, val)
 
         print "Sum leverage: %.2f " % reduce(lambda x, y: x+y, map(lambda z: z[1], leverages))
-
 
 
 if __name__ == '__main__':
