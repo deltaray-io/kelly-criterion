0b3c2828a450d6763ceb42a427358c76f36f190a
Tibor Kiss <tibor.kiss@gmail.com>
2019-02-16 11:39:39 +0100

    ENH: Move to PBR based versioning

--- setup.py
+++ setup.py
@@ -2,22 +2,8 @@
 
 from setuptools import setup
 
-setup(name='kelly_criterion',
-      version='1.0',
-      author='Tibor Kiss',
-      author_email='tibor.kiss@gmail.com',
-      description='Kelly Criterion Calculator',
-      long_description='Optimal leverage calculation for financial securities using Kelly Criterion',
-      license='BSD 3 Clause License',
-      url='http://github.com/tibkiss/kelly-criterion',
-      py_modules=['kelly_criterion'],
-      classifiers=['Development Status :: 6 - Mature',
-                   'License :: OSI Approved :: BSD License',
-                   'Programming Language :: Python :: 2.7',
-                   'Operating System :: OS Independent',
-                   'Environment :: Console',
-                   'Topic :: Office/Business :: Financial :: Investment',
-      ],
-      keywords='kelly formula criterion finance optimal leverage',
-      install_requires=['numpy>=1.9.2', 'pandas>=0.16.1', 'docopt>=0.6.2'],
-      entry_points={'console_scripts': ['kelly_criterion = kelly_criterion:main']})
+
+setup(
+    setup_requires=['pbr'],
+    pbr=True,
+)
